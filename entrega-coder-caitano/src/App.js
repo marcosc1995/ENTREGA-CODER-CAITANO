eb9580dc693a9dc33604f68f1851ddc359e00285
Marcos Caitano <marcos_c95@outlook.com>
2022-04-27 20:06:18 -0300

    update

---
+++ entrega-coder-caitano/src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header>
+        <nav>
+          <h1>Shop</h1>
+          <ul>
+            <li>elemento 1</li>
+            <li>elemento 2</li>
+            <li>elemento 3</li>
+          </ul>
+        </nav>
+      </header>
+    </div>
+  );
+}
+
+export default App;
