d30f5a134edcc95588fdccadcf01243165b505de
Marcos <marcos_c95@outlook.com>
2022-04-28 00:05:09 -0300

    update

--- entrega-coder-caitano/src/App.js
+++ entrega-coder-caitano/src/App.js
@@ -1,24 +1,12 @@
 import logo from "./logo.svg";
 import "./App.css";
+import NavBar from "./components/navBar";
 
 function App() {
   return (
     <div>
-      <header>
-        <nav>
-          <h1>E-ShopTech</h1>
-          <ul>
-            <li>
-              <a href="">Categorias</a>
-            </li>
-            <li>
-              <a href="">Sobre Nosotros</a>
-            </li>
-            <li>
-              <a href="">Contacto</a>
-            </li>
-          </ul>
-        </nav>
+      <header>        
+        <NavBar></NavBar>        
       </header>
     </div>
   );
---
+++ entrega-coder-caitano/src/components/navBar.js
@@ -0,0 +1,21 @@
+const NavBar = ()=>{
+    return (
+        <nav>
+          <h1>E-ShopTech</h1>
+          <ul>
+            <li>
+              <a href="">Categorias</a>
+            </li>
+            <li>
+              <a href="">Sobre Nosotros</a>
+            </li>
+            <li>
+              <a href="">Contacto</a>
+            </li>
+          </ul>
+        </nav>
+    );
+
+}
+
+export default NavBar
