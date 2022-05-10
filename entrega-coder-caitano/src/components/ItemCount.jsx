edbda4ac9d3ebdc9bb02a21d6da873c0d1350031
Marcos <marcos_c95@outlook.com>
2022-05-09 22:37:26 -0300

    update

--- entrega-coder-caitano/src/components/ItemCount.jsx
+++ entrega-coder-caitano/src/components/ItemCount.jsx
@@ -1,27 +1,52 @@
 import { useState, useEffect } from "react";
 
-const ItemCount = (props) =>{
-    const [state, setState] = useState(0)
+const ItemCount = ({ stock, initial }) => {
+  const [count, setCount] = useState(initial);
+  const [totalStock, setTotalStock] = useState(stock);
 
-    const add = ()=>{
-        setState(state + 1)
+  const add = () => {
+    if (count < totalStock) {
+      setCount(count + 1);
+    } else {
+      console.log("No puedorrrr");
     }
+  };
 
-    const res = ()=>{
-        setState(state - 1)
+  const res = () => {
+    if (count > 0) {
+      setCount(count - 1);
+    } else {
+      console.log("Tampoco puedoorrrr");
     }
+  };
 
-    return(
-        <div className="position-relative p-2 text-center">
-            {/* <p>{props.productName}</p> */}
-            <div className="d-flex justify-content-between border">
-                <button onClick={res} className="btn">-</button>
-                <span>{state}</span>
-                <button onClick={add} className="btn">+</button>
-            </div>
-            <button className="btn btn-outline-primary w-100">Agrregar al carrito</button>
-        </div>
-    )
-}
+  const updateStock = () => {
+    setTotalStock(totalStock - count);
+    setCount(1)
+  };
+
+  //   useEffect(() => {
+  //     setStock(() => stock - count);
+  //   }, [count]);
+
+  return (
+    <div className="position-relative p-2 text-center">
+      {/* <p>{props.productName}</p> */}
+      <div className="d-flex justify-content-between border">
+        <button onClick={res} className="btn">
+          -
+        </button>
+        <span>{count}</span>
+        <button onClick={add} className="btn">
+          +
+        </button>
+      </div>
+      <p>Stock {totalStock}</p>
+      <button onClick={updateStock} className="btn btn-outline-primary w-100">
+        Agrregar al carrito
+      </button>
+    </div>
+  );
+};
 
 export default ItemCount;
--- entrega-coder-caitano/src/components/product.jsx
+++ entrega-coder-caitano/src/components/product.jsx
@@ -4,10 +4,10 @@ const Product = (props) => {
   return (
     <div className="card w-25 m-1">
       <img className="img-fluid card-img-top" src={props.img} />
-      <h2 className="text-secondary m-1">{props.title}</h2>
+      <h2 className="text-secondary m-1 fs-6">{props.title}</h2>
       <h2 className="text-end text-secondary m-1">${props.price}</h2>
       {/* <p>{props.description}</p> */}
-      <ItemCount productName={props.title} productAddNumber={1}></ItemCount>
+      <ItemCount stock={5} initial={1}></ItemCount>
     </div>
   );
 };
